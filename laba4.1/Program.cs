3a853a14213b92bcae80b0c6ef143a6d17cd22ae
danil <dddd@gmail.com>
2024-01-31 16:41:19 +0300

    Добавьте файлы проекта.

---
+++ laba4.1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+    Console.Write("n = ");
+    int n = int.Parse(Console.ReadLine());
+    double S = 0;
+    for (int i = 1; i <= 7; i++)
+    {
+        double P = 1;
+        for (int j = 1; j <= 7; j++)
+        {
+            P *= Math.Cos(i + 2 * j);
+        }
+        S += S + P;
+    }
+    Console.WriteLine($"Сумма произведений= {S}");
